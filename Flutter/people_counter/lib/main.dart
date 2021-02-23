726b155adaa1cd429d5adec3c0fbd254813220a1
SirWez <_weslley@ufpi.edu.br>
2021-02-23 17:44:00 -0300

    Stack, images

--- Flutter/people_counter/lib/main.dart
+++ Flutter/people_counter/lib/main.dart
@@ -2,38 +2,51 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "People Counter",
-    home: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text("People: 0",
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-        Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  FlatButton(
-                      child: Text("-1",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontStyle: FontStyle.normal,
-                              fontSize: 20.0)),
-                      onPressed: () {}),
-                  FlatButton(
-                      child: Text("+1",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontStyle: FontStyle.normal,
-                              fontSize: 20.0)),
-                      onPressed: () {}),
-                ])),
-        Text("You can enter pcr !!",
-            style: TextStyle(
-                color: Colors.pink[900],
-                fontStyle: FontStyle.italic,
-                fontSize: 20.0))
-      ],
-    ),
-  ));
+      title: "People Counter",
+      home: Stack(
+        children: <Widget>[
+          Image.asset(
+            "images/restaurant.jpg",
+            fit: BoxFit.cover,
+            height: 1000.0,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("People: 0",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold)),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: FlatButton(
+                          child: Text("+1",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontStyle: FontStyle.normal,
+                                  fontSize: 20.0)),
+                          onPressed: () {},
+                        )),
+                    Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: FlatButton(
+                          child: Text("-1",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontStyle: FontStyle.normal,
+                                  fontSize: 20.0)),
+                          onPressed: () {},
+                        ))
+                  ]),
+              Text("You can enter pcr !!",
+                  style: TextStyle(
+                      color: Colors.pink[900],
+                      fontStyle: FontStyle.italic,
+                      fontSize: 20.0))
+            ],
+          ),
+        ],
+      )));
 }
