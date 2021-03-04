796b229b368bd72794d191a98d8e2b2783e44b77
SirWez <_weslley@ufpi.edu.br>
2021-03-04 20:38:37 -0300

    Calculadora IMC

--- Flutter/calc_imc/lib/main.dart
+++ Flutter/calc_imc/lib/main.dart
@@ -35,26 +35,47 @@ class _HomeState extends State<Home> {
         ],
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(Icons.person_outline,
-              size: 120.0, color: Colors.purple.shade800),
-          TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Peso (kg)",
-                  labelStyle: TextStyle(color: Colors.purple.shade800)),
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.purple, fontSize: 25.0)),
-          TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Altura (cm)",
-                  labelStyle: TextStyle(color: Colors.purple.shade800)),
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.purple, fontSize: 25.0)),
-        ],
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(Icons.person_outline,
+                size: 120.0, color: Colors.purple.shade800),
+            TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Peso (kg)",
+                    labelStyle: TextStyle(color: Colors.purple.shade800)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.purple, fontSize: 25.0)),
+            TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Altura (cm)",
+                    labelStyle: TextStyle(color: Colors.purple.shade800)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.purple, fontSize: 25.0)),
+            Padding(
+              padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
+              child: Container(
+                height: 50.0,
+                child: RaisedButton(
+                  child: Text("CALCULE",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontStyle: FontStyle.normal,
+                          fontSize: 20.0)),
+                  onPressed: () {},
+                  color: Colors.purple,
+                ),
+              ),
+            ),
+            Text("Info",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.purple, fontSize: 20.0))
+          ],
+        ),
       ),
     );
   }
